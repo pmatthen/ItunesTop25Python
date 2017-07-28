483d944857587277eef7f66f6e4828ec0d099f0b
Poulose Matthen <matthen@Pouloses-iMac.Dlink>
2017-07-28 19:28:12 +0530

    first commit

---
+++ ItunesTopSongs.py
@@ -0,0 +1,12 @@
+import urllib, json
+
+url = "https://rss.itunes.apple.com/api/v1/us/itunes-music/top-songs/25/explicit/json"
+response = urllib.urlopen(url)
+data = json.loads(response.read())
+
+resultsList = data['feed']['results']
+
+for index in range(len(resultsList)):
+    resultsListDictionary = resultsList[index]
+    print("{}. {} - {}".format((index+1), resultsListDictionary['artistName'], resultsListDictionary['name']))
+    print "\n"
